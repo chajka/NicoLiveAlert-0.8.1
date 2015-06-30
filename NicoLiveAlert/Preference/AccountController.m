2b0053cb0f3426039f1c083e766762b299836c5a
Чайка <mellanie335@gmail.com>
2015-06-30 22:21:56 +0900

    Add class AccountController into project.

---
+++ NicoLiveAlert/Preference/AccountController.m
@@ -0,0 +1,22 @@
+//
+//  AccountController.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 6/30/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "AccountController.h"
+
+@interface AccountController ()
+
+@end
+
+@implementation AccountController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
