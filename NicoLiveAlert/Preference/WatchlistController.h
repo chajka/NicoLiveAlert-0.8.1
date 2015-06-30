9fc84a281060ddb5a6d1a12de3d14074310b9faf
Чайка <mellanie335@gmail.com>
2015-06-30 22:20:15 +0900

    Add class WatchlistController into project.

---
+++ NicoLiveAlert/Preference/WatchlistController.h
@@ -0,0 +1,13 @@
+//
+//  WatchlistController.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 6/30/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WatchlistController : NSViewController
+
+@end
---
+++ NicoLiveAlert/Preference/WatchlistController.m
@@ -0,0 +1,22 @@
+//
+//  WatchlistController.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 6/30/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "WatchlistController.h"
+
+@interface WatchlistController ()
+
+@end
+
+@implementation WatchlistController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
