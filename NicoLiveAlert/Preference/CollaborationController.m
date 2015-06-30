f1bdd80d1f8d2e394014e90563bec4e2822b971e
Чайка <mellanie335@gmail.com>
2015-06-30 22:26:42 +0900

    Add class CollaborationController into project.

---
+++ NicoLiveAlert/Preference/CollaborationController.m
@@ -0,0 +1,22 @@
+//
+//  CollaborationController.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 6/30/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "CollaborationController.h"
+
+@interface CollaborationController ()
+
+@end
+
+@implementation CollaborationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
