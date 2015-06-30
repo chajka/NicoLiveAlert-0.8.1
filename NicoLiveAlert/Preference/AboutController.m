11af77841dde3e6181b7e3c512c972abe7fdf47f
Чайка <mellanie335@gmail.com>
2015-06-30 22:30:21 +0900

    Add AboutController into project.

---
+++ NicoLiveAlert/Preference/AboutController.m
@@ -0,0 +1,22 @@
+//
+//  AboutController.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 6/30/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "AboutController.h"
+
+@interface AboutController ()
+
+@end
+
+@implementation AboutController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
