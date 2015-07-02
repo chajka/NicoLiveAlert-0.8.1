69077e6782abe970049f0e60e7ac1340114a3221
Чайка <mellanie335@gmail.com>
2015-07-02 19:56:47 +0900

    Create new class WatchItemsManager into project.

---
+++ NicoLiveAlert/Program Management/WatchItemsManager.h
@@ -0,0 +1,13 @@
+//
+//  WatchItemsManager.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/2/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WatchItemsManager : NSObject
+
+@end
---
+++ NicoLiveAlert/Program Management/WatchItemsManager.m
@@ -0,0 +1,13 @@
+//
+//  WatchItemsManager.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/2/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "WatchItemsManager.h"
+
+@implementation WatchItemsManager
+
+@end
