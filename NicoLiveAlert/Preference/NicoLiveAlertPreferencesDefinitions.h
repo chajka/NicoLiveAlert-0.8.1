86acd1de400d419422223811804f79112f22f1bd
Чайка <mellanie335@gmail.com>
2015-07-02 15:45:26 +0900

    Add Files NicoLiveAlertPreferencesDefinitions.h and UsersDefault.plist into project.

---
+++ NicoLiveAlert/Preference/NicoLiveAlertPreferencesDefinitions.h
@@ -0,0 +1,13 @@
+//
+//  NicoLiveAlertPreferencesDefinitions.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/2/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#ifndef NicoLiveAlert_NicoLiveAlertPreferencesDefinitions_h
+#define NicoLiveAlert_NicoLiveAlertPreferencesDefinitions_h
+
+
+#endif
