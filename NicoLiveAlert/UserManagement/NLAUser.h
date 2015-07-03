daf75f7c95ab3be8472275b3d5348320d832fb1f
Чайка <mellanie335@gmail.com>
2015-07-03 19:52:27 +0900

    Implement class NLAUser into project.

---
+++ NicoLiveAlert/UserManagement/NLAUser.h
@@ -0,0 +1,13 @@
+//
+//  NLAUser.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLAUser : NSObject
+
+@end
---
+++ NicoLiveAlert/UserManagement/NLAUser.m
@@ -0,0 +1,13 @@
+//
+//  NLAUser.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/3/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "NLAUser.h"
+
+@implementation NLAUser
+
+@end
