41dd0934d96b37cd3232a852aabcb5165e92c070
Чайка <mellanie335@gmail.com>
2015-07-04 12:17:55 +0900

    Add class NLAUsers into project.

---
+++ NicoLiveAlert/UserManagement/NLAUsers.h
@@ -0,0 +1,13 @@
+//
+//  NLAUsers.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/4/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLAUsers : NSObject
+
+@end
---
+++ NicoLiveAlert/UserManagement/NLAUsers.m
@@ -0,0 +1,13 @@
+//
+//  NLAUsers.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/4/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "NLAUsers.h"
+
+@implementation NLAUsers
+
+@end
